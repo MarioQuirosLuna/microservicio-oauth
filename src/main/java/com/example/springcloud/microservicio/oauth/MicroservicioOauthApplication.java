cdc5e81bb5226f1633034dc9f1f70e724b834293
MarioQL <mquiros@epromcr.com>
2025-01-10 07:57:41 -0600

    Creacion de servicio para autentificacion

---
+++ src/main/java/com/example/springcloud/microservicio/oauth/MicroservicioOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.microservicio.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcloud/microservicio/oauth/microservicio_oauth/MicroservicioOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.microservicio.oauth.microservicio_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
